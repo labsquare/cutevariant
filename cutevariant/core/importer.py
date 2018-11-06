da65e5c2b73e23e657bc9d8ec8a467cd62806df1
dridk <sacha@labsquare.org>
2018-11-06 19:16:26 +0100

    progress on Import Wizard

--- cutevariant/core/importer.py
+++ cutevariant/core/importer.py
@@ -13,8 +13,7 @@ class ImportTask(QRunnable):
 		self.filename = filename
 		self.db_filename = db_filename
 
-	def async_run(self):
-		QThreadPool.globalInstance().run(self)
+		
 
 
 	def run(self):
@@ -65,7 +64,6 @@ class ImportTask(QRunnable):
 					model.Variant.insert_many(chunk).execute()
 					chunk.clear()
 
-			print(chunk)
 			model.Variant.insert_many(chunk).execute()
 
 
--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import peewee
 import sys 
 from cutevariant.gui import *
 from cutevariant.gui.Test import VariantModel
+from cutevariant.core import model
 
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 
-task = ImportTask("/home/sacha/test2.vcf", "test.db")
+database = peewee.SqliteDatabase("test.db")
+model.db.initialize(database)
 
+for v in model.Variant.select():
+	print(v.chrom)
