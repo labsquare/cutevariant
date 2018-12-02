677e2c094df188db5ce96d8d190bc1c311ae7e69
dridk <sacha@labsquare.org>
2018-12-03 00:43:50 +0100

    change view method

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 import sqlalchemy
-
+from sqlalchemy import text
 from cutevariant.core.importer import import_file, import_bed
 from cutevariant.core.model import create_session, Variant, select_variant
 from cutevariant.gui.variantview import * 
@@ -21,6 +21,17 @@ if __name__ == "__main__":
 
 
     import_file("exemples/test.csv", engine)
+
+    session = create_session(engine)
+
+    q = session.query(Variant).join(VariantSet).join(View).filter(text("views.name = 'test'"))
+
+
+    for i in q:
+        print(i.pos)
+
+    # for i in session.query(Variant).filter(sqlalchemy.text("id > 4")):
+    #     print(i.pos)
 
     # import_bed("exemples/gene.bed", engine)
 
--- cutevariant/core/model.py
+++ cutevariant/core/model.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float, Boolean, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import event
+from sqlalchemy import event, ForeignKey
 from sqlalchemy_views import CreateView, DropView
 
 import os
@@ -50,51 +50,30 @@ class Variant(Base):
         Variant.create_column(field.name, Column(column_type))
 
 
-class VariantView(Base):
-    __tablename__ = "variantviews"
+class View(Base):
+    __tablename__ = "views"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
     sql = Column(String)
 
-    def union(self, other):
-        view = VariantView()
-        view.sql = f"{self.sql} UNION {other.sql}"
-        return view
-
-    def intersection(self, other):
-        view = VariantView()
-        view.sql = f"{self.sql} INTERSECT {other.sql}"
-        return view
-
-    def difference(self, other):
-        view = VariantView()
-        view.sql = f"{self.sql} EXCEPT {other.sql}"
-        return view
-
-    def in_region(self):
-        pass
-
-    def __add__(self, other):
-        return self.union(other)
-
-    def __sub__(self, other):
-        return self.difference(other)
-
-    def __and__(self, other):
-        return self.intersection(other)
+class VariantSet(Base):
+    __tablename__ = "view_has_variants"
+    variant_id = Column(Integer, ForeignKey('variants.id'), primary_key=True)
+    view_id = Column(Integer, ForeignKey('views.id'), primary_key=True)
 
 
-@event.listens_for(VariantView, "before_insert")
+
+@event.listens_for(View, "before_insert")
 def create_view(mapper, connect, target):
     print("before insert ", target)
-    connect.execute(f"CREATE VIEW {target.name} AS {target.sql}")
+    #connect.execute(f"CREATE VIEW {target.name} AS {target.sql}")
 
 
-@event.listens_for(VariantView, "before_delete")
-def drop_view(mapper, connect, target):
-    print("before remove ", target)
-    connect.execute(f"Drop VIEW {target.name}")
+# @event.listens_for(VariantView, "before_delete")
+# def drop_view(mapper, connect, target):
+#     print("before remove ", target)
+#     connect.execute(f"Drop VIEW {target.name}")
 
 
 def select_variant(tablename, condition, engine):
