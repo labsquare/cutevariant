cddf84ea3c8e1c435bcbc873a054eeff54deb66d
dridk <sacha@labsquare.org>
2018-12-02 19:49:25 +0100

    blackify

--- cutevariant/core/model.py
+++ cutevariant/core/model.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Column,Integer,String,Float,Boolean, Table
+from sqlalchemy import Column, Integer, String, Float, Boolean, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import event
 from sqlalchemy_views import CreateView, DropView
 
-import os 
+import os
 
 Base = declarative_base()
 
 
 class Field(Base):
     __tablename__ = "fields"
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     category = Column(String)
     description = Column(String)
@@ -21,86 +21,81 @@ class Field(Base):
 
 class Region(Base):
     __tablename__ = "regions"
-    id  = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     bin = Column(Integer)
     chr = Column(String)
     pos = Column(Integer)
     name = Column(String)
 
 
-
-
 class Variant(Base):
-    __tablename__="variants"
-    id =  Column(Integer,primary_key=True)
+    __tablename__ = "variants"
+    id = Column(Integer, primary_key=True)
     bin = Column(Integer)
 
     def __getitem__(self, index):
         return getattr(self, index)
 
-    def __setitem__(self,index, value):
+    def __setitem__(self, index, value):
         setattr(self, index, value)
 
-    @classmethod    
-    def create_column(cls,name,column):
+    @classmethod
+    def create_column(cls, name, column):
         setattr(cls, name, column)
 
     @classmethod
-    def create_column_from_field(cls,field: Field):
-        column_type = eval(field.value_type) # Integer,String,Float,Boolean
+    def create_column_from_field(cls, field: Field):
+        column_type = eval(field.value_type)  # Integer,String,Float,Boolean
         Variant.create_column(field.name, Column(column_type))
 
-    
+
 class VariantView(Base):
-    __tablename__="variantviews"
-    id = Column(Integer,primary_key=True)
+    __tablename__ = "variantviews"
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
     sql = Column(String)
 
-
     def union(self, other):
         view = VariantView()
-        view.sql = f'{self.sql} UNION {other.sql}'
+        view.sql = f"{self.sql} UNION {other.sql}"
         return view
 
     def intersection(self, other):
         view = VariantView()
-        view.sql = f'{self.sql} INTERSECT {other.sql}'
+        view.sql = f"{self.sql} INTERSECT {other.sql}"
         return view
 
     def difference(self, other):
         view = VariantView()
-        view.sql = f'{self.sql} EXCEPT {other.sql}'
+        view.sql = f"{self.sql} EXCEPT {other.sql}"
         return view
-
 
     def __add__(self, other):
         return self.union(other)
 
-    def __sub__(self,other):
+    def __sub__(self, other):
         return self.difference(other)
 
-    def __and__(self,other):
+    def __and__(self, other):
         return self.intersection(other)
 
 
-
-@event.listens_for(VariantView, 'before_insert')
+@event.listens_for(VariantView, "before_insert")
 def create_view(mapper, connect, target):
-    print("before insert ",target)
-    connect.execute(f"CREATE view {target.name} AS {target.sql}")
+    print("before insert ", target)
+    connect.execute(f"CREATE VIEW {target.name} AS {target.sql}")
 
-@event.listens_for(VariantView, 'before_delete')
+
+@event.listens_for(VariantView, "before_delete")
 def drop_view(mapper, connect, target):
-    print("before remove ",target)
-    connect.execute(f"Drop view {target.name}")
+    print("before remove ", target)
+    connect.execute(f"Drop VIEW {target.name}")
 
 
 def create_variant_table(tablename, engine):
-    table = Table(tablename,Base.metadata, autoload=True, autoload_with=engine)
+    table = Table(tablename, Base.metadata, autoload=True, autoload_with=engine)
     return table
-
 
 
 def create_session(engine):
@@ -109,18 +104,8 @@ def create_session(engine):
     return session
 
 
-
-
 if __name__ == "__main__":
 
     pass
 
-
-    #import_file("../../exemples/test2.vcf", "/tmp/test4.db")
-
-
-
-
-
-
-
+    # import_file("../../exemples/test2.vcf", "/tmp/test4.db")
