d3d761298671b57643373e4b155ac093b1274b65
dridk <sacha@labsquare.org>
2018-12-02 22:05:20 +0100

    create import bed

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ import os
 
 import sqlalchemy
 
-from cutevariant.core.importer import import_file
+from cutevariant.core.importer import import_file, import_bed
 from cutevariant.core.model import create_session, Variant, select_variant
 from cutevariant.gui.variantview import * 
 
@@ -22,10 +22,13 @@ if __name__ == "__main__":
 
     import_file("exemples/test.csv", engine)
 
-    app = QApplication(sys.argv)
-    w = VariantView()
-    w.load(engine)
+    import_bed("exemples/gene.bed", engine)
 
-    w.show()
 
-    app.exec_()
+    # app = QApplication(sys.argv)
+    # w = VariantView()
+    # w.load(engine)
+
+    # w.show()
+
+    # app.exec_()
