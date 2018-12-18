8c553d2b6182dac9a3cd1c94a0c6664976e53ba4
dridk <sacha@labsquare.org>
2018-12-19 00:18:53 +0100

    update querybuilder

--- __main__.py
+++ __main__.py
@@ -3,9 +3,10 @@ from PySide2.QtCore import *
 import sys
 import os
 import sqlalchemy
-from sqlalchemy import text
+from sqlalchemy import text, select
 from cutevariant.core.importer import import_file, open_db
 from cutevariant.gui.variantview import *
+from cutevariant.core.query import QueryBuilder
 
 if __name__ == "__main__":
 
@@ -15,13 +16,15 @@ if __name__ == "__main__":
 
     import_file("exemples/test.csv", path)
 
-    open_db(path)
+    engine = sqlalchemy.create_engine(f"sqlite:///{path}", echo=True)
+    meta   = sqlalchemy.MetaData(bind=engine)
 
-    print(Variant)
+    builder = QueryBuilder(engine)
 
-
-
-
+    builder2 = QueryBuilder(engine)
+    for i in builder2.query():
+    	print(i)
+	
 
 
 
