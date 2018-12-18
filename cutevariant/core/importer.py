7a4ca5dc2b0017c012771efe9d7c03136b8f4681
dridk <sacha@labsquare.org>
2018-12-18 23:53:51 +0100

    use sqlalchemy core instead

--- cutevariant/core/importer.py
+++ cutevariant/core/importer.py
@@ -1,43 +1,58 @@
-from sqlalchemy import create_engine
+from sqlalchemy import *
+from sqlalchemy.ext.automap import automap_base
 import os
 import csv
-
 from .readerfactory import ReaderFactory
-from .model import *
 
 
-def import_file(filename, engine):
+def import_file(filename, dbpath):
 
-    session = create_session(engine)
+    try:
+        os.remove(dbpath)
+    except:
+        pass
 
-    # Create tables 
-    Field.__table__.create(engine)
-    Selection.__table__.create(engine)
-    selection_has_variant_table.create(engine)
-
-    # get reader 
+    engine = create_engine(f"sqlite:///{dbpath}", echo=True)
+    metadata = MetaData(bind=engine)
+    # # get reader 
     reader = ReaderFactory.create_reader(filename)
 
-    # extract fields and create Variant table 
-    for data in reader.get_fields():
-        session.add(Field(**data))
-        Variant.create_column_from_field(Field(**data))
+    # # extract fields for variants table 
+    columns = [Column(field["name"], eval(field["value_type"])) for field in reader.get_fields()]
 
-    # Create table 
-    Variant.__table__.create(engine)
+    print(columns)
 
-    session.commit()
+    variant_table = Table("variants", metadata,Column('id', Integer, primary_key=True), *columns)
+    fields_table  = Table("fields", metadata,Column('id', Integer, primary_key=True) )
 
-    # load variant 
-    variant_count = 0
-    for i, data in enumerate(reader.get_variants()):
-        variant = Variant(**data)
-        session.add(variant)
-        variant_count += 1
-    session.commit()
 
-    # Create default selection 
-    session.add(Selection(name="all", description="all variant", count = variant_count))
-    session.add(Selection(name="favoris", description="favoris", count = 0))
+    metadata.create_all()
+
+
+    insert = variant_table.insert()
+
+    cache = []
+    for variant in reader.get_variants():
+        cache.append(variant)
+
+    engine.execute(insert, cache) 
+
+
+
+def open_db(dbpath):
+    engine = create_engine(f"sqlite:///{dbpath}", echo=True)
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Variants  = Base.classes.variants
     
-    session.commit()
+
+
+
+
+
+
+    # # Create default selection 
+    # session.add(Selection(name="all", description="all variant", count = variant_count))
+    # session.add(Selection(name="favoris", description="favoris", count = 0))
+    
+    # session.commit()
