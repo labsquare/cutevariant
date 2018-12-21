9444ec21b339af117028d30b8b4fd2a7b946eb65
dridk <sacha@labsquare.org>
2018-12-21 01:42:07 +0100

    add view test

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ from PySide2.QtCore import *
 import sys
 import os
 from cutevariant.core.importer import import_file
-# from cutevariant.gui.variantview import *
+from cutevariant.gui.variantview import VariantView
 from cutevariant.core.query import QueryBuilder
 from cutevariant.core.model import Selection, Variant
 import sqlite3
@@ -18,11 +18,16 @@ if __name__ == "__main__":
 
     conn   = sqlite3.connect(path)
 
-    builder = QueryBuilder(conn)
-    builder.fields = ["chr","pos"]
+    query = QueryBuilder(conn)
 
-    for i in builder.query():
-        print(i)
+    app = QApplication(sys.argv)
+
+    view = VariantView()
+    view.load(query)
+    view.show()
+
+    app.exec_()
+
 
 
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,18 +1,45 @@
 import pytest
 import sqlite3
-from cutevariant.core.model import * 
+import os
+from cutevariant.core.model import Selection, Field, Variant 
 from cutevariant.core.importer import import_file
+from .utils import table_exists, table_count
 
 
-def table_exists(name, conn):
-	c = conn.cursor()
-	c.execute("SELECT name FROM sqlite_master WHERE name = 'f{name}'")
-	print(c.fetchone())
+@pytest.fixture
+def conn():
+    db_path = "/tmp/test_cutevaiant_model.db"
+    try:
+        os.remove(db_path)
+    except:
+        pass
+
+    return sqlite3.connect(db_path)
 
 
-def test_creation():
-	conn = sqlite3.connect("/tmp/test_cutevariant.db")
+def test_selection(conn):
+    #import_file("exemples/test.csv", db_path)
+    
+    Selection(conn).create()
+    assert table_exists("selections",conn), "table selection cannot be create"
+    assert table_exists("selection_has_variant",conn), "table selection_has_variant cannot be create"
 
-	table_exists("variants", conn)
+    Selection(conn).insert({
+        "name": "test",
+        "count": 100,
+        "truc": 44
+        })
 
- 
+    assert table_count("selections",conn) == 1
+
+    conn.close()
+
+
+
+def test_field(conn):
+    #import_file("exemples/test.csv", db_path)
+    
+    Field(conn).create()
+    assert table_exists("fields",conn), "table fields cannot be create"
+
+    conn.close()
