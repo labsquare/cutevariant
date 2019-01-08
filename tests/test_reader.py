5c64b92fbad065f91910b854c56b51d4f1286454
sacha schutz <sacha@labsquare.org>
2019-01-08 23:33:25 +0100

    update test_reader

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,49 +1,68 @@
 
-from cutevariant.core.reader import CsvReader, VcfReader
+from cutevariant.core.reader import VcfReader
+import vcf
 import os 
-
-def test_csv():
-	filename = "exemples/test.csv"
-	assert os.path.exists(filename), "file doesn't exists"
-
-	with open(filename, "r") as file:
-		reader = CsvReader(file)
-		fields = [f["name"] for f in reader.get_fields()]
-		
-		assert "chr" in fields
-		assert "pos" in fields
-		assert "ref" in fields
-		assert "alt" in fields
-
 
 def test_vcf():
 	filename = "exemples/test.vcf"
-	assert os.path.exists(filename), "file doesn't exists"
+	#assert os.path.exists(filename), "file doesn't exists"
 
+	MAX_VARIANTS = 10
+	GENOTYPE = {
+	"1/1" : 2,
+	"1/0" : 1,
+	"0/0": 0
+	}
+
+	# Import using pyvcf 
+	with open(filename, 'r') as file:
+		other_reader = vcf.Reader(file)
+		fields = [i for i in other_reader.infos] # Plus tard
+
+		# Take some variants
+		other_variants = []
+		for i, variant in enumerate(other_reader):
+			other_variants.append(variant)
+			if i >= MAX_VARIANTS:
+				break
+
+		
+
+	# import using cutevariant 
 	with open(filename, "r") as file:
-		reader = VcfReader(file)
-		fields = [f["name"] for f in reader.get_fields()]
+		my_reader = VcfReader(file)
+
+		assert my_reader.get_samples() == other_reader.samples
+
+		fields = [f["name"] for f in my_reader.get_fields()]
 		
 		assert "chr" in fields
 		assert "pos" in fields
 		assert "ref" in fields
 		assert "alt" in fields
 
+		# TODO : test annotation .. Gloups .. 
 
-def test_A():
-	filename = "exemples/test.vcf"
-	assert os.path.exists(filename), "file doesn't exists"
+		# Take some variants
+		my_variants = []
+		for i, variant in enumerate(my_reader.get_variants()):
+			my_variants.append(variant)
+			if i >= MAX_VARIANTS:
+				break
 
-	with open(filename, "r") as file:
-		reader = VcfReader(file)
-		fields = [f["name"] for f in reader.get_fields()]
-		
-		assert "chr" in fields
-		assert "pos" in fields
-		assert "ref" in fields
-		assert "alt" in fields
+		# test each variants 
+		for i, variant in enumerate(my_variants):
+			assert variant["chr"] == other_variants[i].CHROM
+			assert variant["pos"] == other_variants[i].POS
+			assert variant["ref"] == other_variants[i].REF
 
-		#print(*reader.get_fields())
+			# Test if samples exists 
+			if len(other_variants[i].samples) != 0:
+				other_sample = other_variants[i].samples[0].sample
+				other_gt     = other_variants[i].samples[0]["GT"]
+
+				assert variant["samples"][0]["gt"] == GENOTYPE.get(other_gt,-1)
+
 
 
 
