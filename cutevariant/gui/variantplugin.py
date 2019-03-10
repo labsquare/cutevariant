c37207ff60a8e0c61014b414c9592d486226cf4b
dridk <sacha@labsquare.org>
2019-03-10 17:23:46 +0100

    testing plugins

---
+++ cutevariant/gui/variantplugin.py
@@ -0,0 +1,14 @@
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+
+class VariantPlugin(QWidget):
+	def __init__(self):
+		super().__init__()
+
+	def set_variant(self, variant):
+		raise NotImplemented()
+
+
+
