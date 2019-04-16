39b89263a40f3e26a892d9ba73c6c67fba80c853
ysard <ysard@users.noreply.github.com>
2019-04-16 03:18:18 +0200

    [gui] Handy class on top of *PluginWidget => usefull for common methods like objectName()

--- cutevariant/gui/plugin.py
+++ cutevariant/gui/plugin.py
@@ -1,10 +1,25 @@
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
-from PySide2.QtGui import * 
+from PySide2.QtGui import *
 from cutevariant.core import Query
 
 
-class QueryPluginWidget(QWidget):
+class PluginWidget(QWidget):
+    """Handy class for methods common to QueryPluginWidget and VariantPluginWidget"""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def objectName(self):
+        """Overrided: Return an object name based on windowTitle
+
+        .. note:: Some plugins don't set objectName attribute and so their state
+            can't be saved with MainWindow's saveState function.
+        """
+        return self.windowTitle().lower()
+
+
+class QueryPluginWidget(PluginWidget):
     """
     This is a base class for all widget which observe a Query
 
@@ -13,9 +28,6 @@ class QueryPluginWidget(QWidget):
 
     #  signals
     changed = Signal()
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
 
     @property
     def query(self):
@@ -26,14 +38,11 @@ class QueryPluginWidget(QWidget):
         raise NotImplemented()
 
 
-class VariantPluginWidget(QWidget):
+class VariantPluginWidget(PluginWidget):
     """
-    This is a base class for all widget which get variant data 
+    This is a base class for all widget which get variant data
     when clicking on it from the main view
     """
-    def __init__(self):
-        super().__init__()
 
     def set_variant(self, variant):
         raise NotImplemented()
-
