6af7b36b046c2190bbbcef4743003c7017aa128b
ysard <ysard@users.noreply.github.com>
2019-04-18 00:49:09 +0200

    [gui] missing file for #39

---
+++ cutevariant/gui/style.py
@@ -0,0 +1,9 @@
+"""A place to store style rules for the GUI"""
+
+TYPE_COLORS = {
+    "str": "#27A4DD",  # blue
+    "bool": "#F1646C",  # red
+    "float": "#9DD5C0",  # light blue
+    "int": "#FAC174",  # light yellow
+    "NoneType": "#FFFFFF",  # white
+}
