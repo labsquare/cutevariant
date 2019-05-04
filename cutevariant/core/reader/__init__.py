eb6b58d183d304256436499fb799e5f2d3cbd0c5
ysard <ysard@users.noreply.github.com>
2019-05-04 22:35:05 +0200

    [doc] add doc of ficon; setFontPath is not a classmethod anymore

--- cutevariant/core/reader/__init__.py
+++ cutevariant/core/reader/__init__.py
@@ -1,3 +1,4 @@
+"""Expose of high-level reader classes"""
 from .vcfreader import VcfReader
 from .csvreader import CsvReader
 from .fakereader import FakeReader
--- cutevariant/gui/ficon.py
+++ cutevariant/gui/ficon.py
@@ -1,24 +1,44 @@
-from PySide2.QtWidgets import * 
-from PySide2.QtCore import * 
-from PySide2.QtGui import * 
+# Qt imports
+from PySide2.QtCore import qApp, Qt, QRect, QPoint
+from PySide2.QtGui import (
+    QIconEngine,
+    QColor,
+    QPainter,
+    QIcon,
+    QFontDatabase,
+    QFont,
+    QPalette,
+    QPixmap,
+    QPen,
+)
 
+# Custom imports
+from cutevariant.commons import logger
+
+LOGGER = logger()
 
 
 class FIconEngine(QIconEngine):
+    """Base class for FIcon; used to load custom font"""
+
+    # Class attribute
+    font = None
 
     def __init__(self):
         super().__init__()
         self.setColor(qApp.palette().color(QPalette.Normal, QPalette.Text))
 
-    def setCharacter(self, hex_character : int):
+    def setCharacter(self, hex_character: int):
         self.hex_character = hex_character
 
     def setColor(self, color: QColor):
         self.color = color
 
-    def paint(self, painter:QPainter, rect:QRect, mode:QIcon.Mode, state:QIcon.State):
-        '''override''' 
-        font = FIconEngine.font if hasattr(FIconEngine, 'font') else painter.font() 
+    def paint(
+        self, painter: QPainter, rect: QRect, mode: QIcon.Mode, state: QIcon.State
+    ):
+        """override"""
+        font = FIconEngine.font if hasattr(FIconEngine, "font") else painter.font()
         painter.save()
 
         if mode == QIcon.Disabled:
@@ -26,42 +46,44 @@ class FIconEngine(QIconEngine):
 
         else:
             painter.setPen(QPen(self.color))
-        
+
         font.setPixelSize(rect.size().width())
 
         painter.setFont(font)
-        #painter.setRenderHint(QPainter.HighQualityAntialiasing, True)
-        painter.drawText(rect, Qt.AlignCenter|Qt.AlignVCenter, str(chr(self.hex_character)))
+        # painter.setRenderHint(QPainter.HighQualityAntialiasing, True)
+        painter.drawText(
+            rect, Qt.AlignCenter | Qt.AlignVCenter, str(chr(self.hex_character))
+        )
         painter.restore()
 
-
-    def pixmap(self,size,mode,state):
+    def pixmap(self, size, mode, state):
         pix = QPixmap(size)
         pix.fill(Qt.transparent)
         painter = QPainter(pix)
-        self.paint(painter, QRect(QPoint(0,0),size), mode, state)
-        return pix;
+        self.paint(painter, QRect(QPoint(0, 0), size), mode, state)
+        return pix
 
     @classmethod
     def setFontPath(cls, filename):
+        """Set font from a font file and return it
+
+        :return: QFont or False if the file is not loaded
+        """
         db = QFontDatabase()
         font_id = db.addApplicationFont(filename)
         if font_id == -1:
-            print("cannot load font icon")
-            False
+            LOGGER.error("FIconEngine:setFontPath:: cannot load font icon.")
+            return False
 
         cls.font = QFont(db.applicationFontFamilies(font_id)[0])
-
-
         return cls.font
 
 
-
-
-
-
 class FIcon(QIcon):
-    def __init__(self, hex_character : int, color = None):
+    """Handy public class to load and use custom font in QIcons"""
+
+    def __init__(self, hex_character: int, color=None):
+        """Build an icon with the given character and color from the current font"""
         self.engine = FIconEngine()
         self.engine.setCharacter(hex_character)
         if color:
@@ -69,7 +91,10 @@ class FIcon(QIcon):
         super().__init__(self.engine)
 
 
-    @classmethod
-    def setFontPath(cls, filename):
-        return FIconEngine.setFontPath(filename)
+def setFontPath(filename):
+    """Handy function to load font file
 
+    .. note:: Fonts are supposed to be in cm.DIR_FONTS
+    .. note:: This function is called only 1 time at the start of the program
+    """
+    return FIconEngine.setFontPath(filename)
