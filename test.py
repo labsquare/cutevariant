779a585be0595cdc09717f34628225c1769b7138
sacha schutz <sacha@labsquare.org>
2019-05-14 01:00:50 +0200

    awesome... I success to write myself the where clause myself ! @aluriak

--- test.py
+++ test.py
@@ -2,8 +2,55 @@ from textx import metamodel_from_file
 
 
 
-mm = metamodel_from_file("test.tx")
+class FilterTerm(object):
+    def __init__(self, **kwargs):
+        self.field = kwargs["field"]
+        self.op = kwargs["op"]
+        self.val = kwargs["val"]
+
+    def __str__(self):
+        return f"{self.field} {self.op} {self.val}"
+
+    @property
+    def value(self):
+        return {"field": self.field, "op": self.op, "value": self.val}
+    
+
+
+class FilterExpression(object):
+    def __init__(self, **kwargs):
+        self.op = kwargs["op"]
+        self.parent = kwargs["parent"]
+
+    @property
+    def value(self):
+        out = []
+        for i in self.op:
+            if type(i) == str:
+                out.append(i)
+            else:
+                out.append(i.value)
+        return out
+
+    
+class FilterOperand(object):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.op = kwargs["op"]
+
+
+    def __str__(self):
+        return str(self.op)
+
+    @property
+    def value(self):
+        return self.op.value
+    
+
+
+
+mm = metamodel_from_file("test.tx",  classes=[FilterTerm,FilterOperand, FilterExpression])
 
 model = mm.model_from_file("test.txt")
 
-print(model)
+print(model.filter.value)
