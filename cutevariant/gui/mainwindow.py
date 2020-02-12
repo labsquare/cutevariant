a86fcadae67ddbca781868084d0fe1280cec9f0e
sacha schutz <sacha@labsquare.org>
2020-02-12 23:57:06 +0100

    remove test script for appveyor

--- cutevariant/gui/mainwindow.py
+++ cutevariant/gui/mainwindow.py
@@ -175,6 +175,13 @@ class MainWindow(QMainWindow):
         )
 
         self.file_menu.addSeparator()
+
+        self.file_menu.addAction(
+            FIcon(), self.tr("Export ... "))
+
+        self.file_menu.addSeparator()
+
+        self.file_menu.addSeparator()
         self.file_menu.addAction(self.tr("&Quit"), qApp.quit, QKeySequence.Quit)
 
         ## Edit
