0718f924f327ff31842f5cb73cc2e8e1c16921fa
ysard <ysard@users.noreply.github.com>
2020-09-19 03:26:40 +0200

    [formatters] add doc; black

--- cutevariant/gui/formatter.py
+++ cutevariant/gui/formatter.py
@@ -1,42 +1,53 @@
-from PySide2.QtCore import Qt,QModelIndex,QRect
-from PySide2.QtWidgets import QStyleOptionViewItem
-from PySide2.QtGui import QFont, QIcon, QColor, QPainter
-from cutevariant.gui.ficon import FIcon
-
+# Standard imports
 import importlib
 import pkgutil
 import os
 import inspect
-import functools
+
+# Qt imports
+from PySide2.QtCore import Qt, QModelIndex, QRect
+from PySide2.QtWidgets import QStyleOptionViewItem
+from PySide2.QtGui import QIcon, QPainter
 
 
 class Formatter(object):
-    """This class helps you to customize cell style from QueryModel.
-    You can set the font, background, foreground and decoration ( QIcon)
-
+    """Helper to customize cell style from QueryModel.
+    You can set the font, background, foreground and decoration (QIcon)
     """
 
     def __init__(self):
         super().__init__()
 
-    def paint(self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex):
-
-
+    def paint(
+        self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex
+    ):
         painter.drawText(option.rect, option.displayAlignment, self.value(index))
 
-    def field_name(self,index: QModelIndex):
+    def field_name(self, index: QModelIndex):
         return index.model().headerData(index.column(), Qt.Horizontal)
 
     def value(self, index: QModelIndex):
         return index.data(Qt.DisplayRole)
 
-
-    def draw_icon(self,painter:QPainter, rect: QRect, icon : QIcon):
+    def draw_icon(self, painter: QPainter, rect: QRect, icon: QIcon):
         r = QRect(0, 0, 20, 20)
         r.moveCenter(rect.center())
         painter.drawPixmap(r, icon.pixmap(20, 20))
 
+
+################################################################################
+
+
 def find_formatters(path=None):
+    """Find and return formatter classes from a directory
+
+    Keyword Arguments:
+        path [str] -- the folder path where plugin are
+
+    Returns:
+        [generator [Formatter]] -- A Formatter class ready to be instantiated
+    """
+
     #  if path is None, return internal plugin path
     if path is None:
         formatter_path = os.path.join(os.path.dirname(__file__), "formatters")
@@ -51,9 +62,11 @@ def find_formatters(path=None):
         module = spec.loader.load_module()
 
         for name, obj in inspect.getmembers(module):
-            if inspect.isclass(obj):
-                if "Formatter" in str(obj.__bases__):
-                    yield obj
+            # if the object is a class, whether built-in or created
+            # => same as isinstance(obj, type)
+            # + test parent class (Formatter)
+            if inspect.isclass(obj) and "Formatter" in str(obj.__bases__):
+                yield obj
 
 
 if __name__ == "__main__":
