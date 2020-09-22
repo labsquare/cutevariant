80c60911c7ddef621c9eca63988707efa6cb6852
ysard <ysard@users.noreply.github.com>
2020-09-22 18:29:17 +0200

    [test:CsvWriter] Please also test the content of your files, not just the ability to write them !

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,31 +1,36 @@
 # Standard imports
 import pytest
-import sqlite3
 import tempfile
-
+import sqlite3
 
 # Custom imports
 from cutevariant.core.importer import import_reader
 from cutevariant.core.reader import FakeReader
 from cutevariant.core.writer import CsvWriter, AbstractWriter
-import sqlite3
 
 
-WRITERS = [
-    CsvWriter(tempfile.NamedTemporaryFile("w"))
-    ]
+WRITERS = [CsvWriter(tempfile.NamedTemporaryFile("w+"))]
 
 
 @pytest.mark.parametrize(
     "writer", WRITERS, ids=[str(i.__class__.__name__) for i in WRITERS]
 )
 def test_writer(writer):
-    
+
     conn = sqlite3.connect(":memory:")
     import_reader(conn, FakeReader())
-    reader = FakeReader()
     writer.save(conn)
+
+    # Test file content
+    writer.device.seek(0)
+    content = writer.device.read()
+    print("Content:", content)
+
+    expected = """1\t11\t125010\tT\tA
+2\t12\t125010\tT\tA
+3\t13\t125010\tT\tA
+"""
+    assert content == expected
+
+    # Delete file
     writer.device.close()
-
-
-
