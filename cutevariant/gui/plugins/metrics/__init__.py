c3c50363325b1919b1a301476b52e04163c54c78
ysard <ysard@users.noreply.github.com>
2020-09-23 02:47:01 +0200

    [plugin:metrics] Move from metric_dialog to metrics (uniformization)

--- cutevariant/gui/plugins/metrics/__init__.py
+++ cutevariant/gui/plugins/metrics/__init__.py
@@ -1,4 +1,4 @@
 __title__ = "Metrics plugin"
-__description__ = " A dialog to compute different kind of metric "
+__description__ = "A dialog to compute different kind of metric "
 __author__ = "Sacha schutz"
 __version__ = "1.0.0"
