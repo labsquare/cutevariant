9976fe3646ee34a70ebea1acf89814116fe4ad81
ysard <ysard@users.noreply.github.com>
2020-09-26 04:58:57 +0200

    [tests:command] almost complete review

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,4 +1,4 @@
-from cutevariant.core import command, sql, vql
+from cutevariant.core import command, sql
 from cutevariant.core.reader import VcfReader
 from cutevariant.core.importer import import_reader
 import pytest
@@ -24,9 +24,8 @@ def test_select_cmd(conn):
 
 
 def test_select_cmd_with_set(conn):
-
+    """Test the select query of gene sets"""
     # Import fake words (gene) as a new set called "test"
-
     geneSets = ["CHID1", "AP2A2"]
     for gene in geneSets:
         conn.execute(f"INSERT INTO sets (name,value) VALUES ('test', '{gene}') ")
@@ -42,63 +41,102 @@ def test_select_cmd_with_set(conn):
 
 
 def test_create_cmd(conn):
-
+    """Test creation of a selection table based on default 'variants' table"""
     result = command.create_cmd(conn, source="variants", target="test")
-
+    print("create_cmd result:", result)
     assert "id" in result
 
+    # Get the id of the created selection
     selection_id = result["id"]
+    # The count of variants in the selection must be the same as in variants table
     source_q = conn.execute(f"SELECT COUNT(id) FROM variants").fetchone()[0]
     target_q = conn.execute(
         f"SELECT COUNT(id) FROM variants INNER JOIN selection_has_variant vs ON variants.id = vs.variant_id AND vs.selection_id = {selection_id}"
     ).fetchone()[0]
-
     assert source_q == target_q
 
 
 def test_count_cmd(conn):
+    """Test variants counting in variants table"""
     result = command.count_cmd(conn, source="variants")
     assert "count" in result
     assert result["count"] == 11
 
 
 def test_drop_cmd(conn):
+    """Test drop command on selections table"""
 
+    # Create a selection named "subset"
     conn.execute("INSERT INTO selections (name) VALUES ('subset')")
     assert "subset" in [
         i["name"] for i in conn.execute("SELECT name FROM selections").fetchall()
     ]
-    command.drop_cmd(conn, feature="selections", name="subset")
+    ret = command.drop_cmd(conn, feature="selections", name="subset")
     assert "subset" not in [
         i["name"] for i in conn.execute("SELECT name FROM selections").fetchall()
     ]
+    assert ret["success"]
 
 
 def test_bed_cmd(conn):
-    command.bed_cmd(conn, source="variants", target="test", path="examples/test.bed")
+    """Test bed file insertion as selection"""
+    ret = command.bed_cmd(conn, source="variants", target="test", path="examples/test.bed")
+    # Number of selections = id of last selection
+    selection_number = conn.execute("SELECT COUNT(*) from selections").fetchone()[0]
+    assert ret["id"] == selection_number
+
+    # Test data in association table
+    variants_in_selection = conn.execute("SELECT COUNT(*) from selection_has_variant").fetchone()[0]
+    # 3 variants are concerned by the bed intervals
+    expected = 3
+    assert variants_in_selection == expected
 
 
 def test_set_cmd(conn):
+    """Test set operation between 2 selections"""
     command.create_cmd(conn, source="variants", target="A")
     command.create_cmd(conn, source="variants", target="B")
-    command.set_cmd(conn, target="C", first="A", second="B", operator="+")
+    selection_C = command.set_cmd(conn, target="C", first="A", second="B", operator="-")
+    # Substraction of A (11 variants) - B (11 variants) results in 0 variant
+    # Since create_selection_from_sql does a rollback, C selection is not present
+    # return is empty
+    assert selection_C == dict()
+    ret = list(command.select_cmd(conn, fields=["chr", "pos", "gene"], source="C"))
+    expected = 0
+    assert len(ret) == expected
+
+    # selection 1: variants
+    # 2: A
+    # 3: B
+    # => C is not created
+    selection_number = conn.execute("SELECT COUNT(*) from selections").fetchone()[0]
+    assert selection_number == 3
+
+    selection_C = command.set_cmd(conn, target="C", first="A", second="B", operator="+")
+    selection_C_id = selection_C["id"]
+    print(selection_C_id)
+    variants_in_selection = conn.execute(f"SELECT DISTINCT COUNT(*) from selection_has_variant WHERE selection_id = {selection_C_id}").fetchone()[0]
+
+    expected = 11
+    assert variants_in_selection == expected
 
 
 def test_import_cmd(conn):
+    # TODO: Rewrite this import method from hasardous files
 
-    TEST_FILE = "examples/gene.txt"
-    NAME = "boby"
+    test_file = "examples/gene.txt"
+    name = "boby"
 
     excepted_word = []
-    with open(TEST_FILE, "r") as file:
+    with open(test_file, "r") as file:
         for word in file:
             excepted_word.append(word.strip())
 
-    command.import_cmd(conn, "sets", NAME, TEST_FILE)
+    command.import_cmd(conn, "sets", name, test_file)
 
     for record in conn.execute("SELECT * FROM sets"):
         item = dict(record)
-        item["name"] == NAME
+        assert item["name"] == name
 
     # assert type(cmd) == command.SelectCommand
     # assert cmd.fields == ["chr","pos"]
@@ -123,7 +161,6 @@ def test_import_cmd(conn):
 
 
 def test_create_command_from_obj(conn):
-    import inspect
 
     partial_fct = command.create_command_from_obj(
         conn,
@@ -152,8 +189,12 @@ def test_create_command_from_obj(conn):
 
 
 def test_execute(conn):
+    """Test the wrapper of create_command_from_obj()
 
-    # Select variant with ref = C
+    Translate VQL string to SQL wrapped query
+    """
+
+    # Select variant with ref = C (4 variants)
     result = command.execute(conn, "CREATE setA FROM variants WHERE ref='C'")
 
     assert "id" in result
@@ -161,26 +202,30 @@ def test_execute(conn):
     for variant in command.execute(conn, "SELECT chr, pos, ref, alt FROM setA"):
         assert variant["ref"] == "C"
 
-    # Select variants with alt = A
+    # Select variants with alt = A (7 variants)
     result = command.execute(conn, "CREATE setB FROM variants WHERE alt ='A'")
     assert "id" in result
     for variant in command.execute(conn, "SELECT chr, pos, ref, alt FROM setB"):
         assert variant["alt"] == "A"
 
-    # Create intersection
+    # Create intersection (3 in common)
     result = command.execute(conn, "CREATE set_inter = setB & setA")
     assert "id" in result
-    for variant in command.execute(conn, "SELECT chr, pos, ref, alt FROM set_inter"):
+    for found, variant in enumerate(command.execute(conn, "SELECT chr, pos, ref, alt FROM set_inter"), 1):
         assert variant["alt"] == "A" and variant["ref"] == "C"
 
+    print("Expected number of variants:", found)
+    assert found == 3
+
     #  Create bedfile
-    BEDFILE = "examples/test.bed"
+    bed_file = "examples/test.bed"
     result = command.execute(
-        conn, f"CREATE set_bed FROM variants INTERSECT '{BEDFILE}' "
+        conn, f"CREATE set_bed FROM variants INTERSECT '{bed_file}' "
     )
     assert "id" in result
 
-    with open(BEDFILE) as file:
+    # Check that variants in intervals were added in DB
+    with open(bed_file) as file:
         reader = csv.reader(file, delimiter="\t")
         for variant in command.execute(conn, "SELECT chr, pos, ref, alt FROM set_bed"):
             is_in = False
@@ -189,8 +234,7 @@ def test_execute(conn):
                 if len(line) == 3:
                     if (
                         str(line[0]) == str(variant["chr"])
-                        and int(variant["pos"]) >= int(line[1])
-                        and int(variant["pos"]) <= int(line[2])
+                        and int(line[1]) <= int(variant["pos"]) <= int(line[2])
                     ):
                         is_in = True
-            assert is_in == True
+            assert is_in
