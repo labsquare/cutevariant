36d060dd425e7ea656a923cb299a7fe35a44965a
ysard <ysard@users.noreply.github.com>
2020-10-08 20:11:07 +0200

    [core] Big improvement: Get size of uncompressed file in gz file in a matter of ms instead of few tens of seconds; #157

--- cutevariant/commons.py
+++ cutevariant/commons.py
@@ -136,5 +136,6 @@ def get_uncompressed_size(filepath):
     """Get the size of the given compressed file
     This size is stored in the last 4 bytes of the file.
     """
-    with gzip.open(filepath, "rb") as file_obj:
-        return file_obj.seek(0, io.SEEK_END)
+    with open(filepath, "rb") as file_obj:
+        file_obj.seek(-4, 2)
+        return int.from_bytes(file_obj.read(4), byteorder='little')
