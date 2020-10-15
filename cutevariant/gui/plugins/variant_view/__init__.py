0a8d16b04358bfc8929587be3d158ab1c30538ab
ysard <ysard@users.noreply.github.com>
2020-10-15 02:39:57 +0200

    [plugin:variant_view] Add long description; #141

--- cutevariant/gui/plugins/variant_view/__init__.py
+++ cutevariant/gui/plugins/variant_view/__init__.py
@@ -1,5 +1,39 @@
 __title__ = "View"
 __description__ = "The default view for variants DB"
-__long_description__ = ""
+__long_description__ = """
+<p>This window is the main window of Cutevariant. Here you will find the variants
+and their related data presented in the various columns selected from the
+"Fields editor" plugin.</p>
+
+You can:
+
+<ul>
+<li>Group the variants according to the columns of your choice.
+This function is interesting if you display the data of sometimes numerous
+annotations for the same variant.
+You will find all the annotations grouped for each variant.</li>
+
+<li><b>Set the ACMG score</b> of a variant in 2 clicks via the context menu.</li>
+
+<li><b>Mark one or more variants as favorite(s)</b> so that you can easily find
+them later.</li>
+
+<li>Edit/add comments.</li>
+
+<li><b>Consult external databases</b> referencing each selected variant to
+quickly obtain additional information.</li>
+
+<li><b>Copy</b> one or more variants into a format that can be used directly in
+a Calc/Excel spreadsheet.</li>
+</ul>
+
+<p>The displayed variants can be <b>controlled by filters</b> modified in real
+time from the "filters editor" plugin or from a VQL query from the "VQL editor"
+plugin.</p>
+
+<p>Each subset of variants thus obtained can be saved as a selection using the
+"Save variants" button.
+The selections are listed in the "source editor" plugin.</p>
+"""
 __author__ = "Sacha schutz"
 __version__ = "1.0.0"
