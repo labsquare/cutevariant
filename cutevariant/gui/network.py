c8378e35f04fa01f6d5d6bc6bde6db3234e4b870
ysard <ysard@users.noreply.github.com>
2020-10-16 18:49:32 +0200

    [core] Network: Fix potential not defined variable

--- cutevariant/gui/network.py
+++ cutevariant/gui/network.py
@@ -1,6 +1,11 @@
 from PySide2.QtCore import QSettings
 from PySide2.QtNetwork import QNetworkAccessManager, QNetworkProxy
 
+from cutevariant.commons import logger
+
+
+LOGGER = logger()
+
 PROXY_TYPES = {
     "No Proxy": QNetworkProxy.NoProxy,
     "Default": QNetworkProxy.DefaultProxy,
@@ -17,7 +22,7 @@ def get_network_manager():
     # Get proxy settings data
     settings = QSettings("labsquare", "cutevariant")
     settings.beginGroup("proxy")
-    p_type_index = settings.value("type")
+    p_type_index = settings.value("type", 0)
     p_host = settings.value("host")
     p_port = settings.value("port", 80)
     p_username = settings.value("username")
@@ -25,8 +30,7 @@ def get_network_manager():
     settings.endGroup()
 
     try:
-        if p_type_index:
-            p_type = list(PROXY_TYPES.values())[int(p_type_index)]
+        p_type = list(PROXY_TYPES.values())[int(p_type_index)]
 
         if p_port:
             p_port = int(p_port)
@@ -34,7 +38,8 @@ def get_network_manager():
         if p_type is not QNetworkProxy.NoProxy:
             proxy = QNetworkProxy(p_type, p_host, p_port, p_username, p_password)
             network_manager.setProxy(proxy)
-    except:
+    except Exception as e:
+        LOGGER.exception(e)
         pass
 
     return network_manager
