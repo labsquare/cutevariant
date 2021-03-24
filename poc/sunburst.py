cbd8b4230fa5618f1956fb0cc6088358536f3cc7
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-03-24 13:09:40 +0100

    Completed proof of concept for SunburstWidget
    Now the sunburst is drawn correctly and supports highlighting

--- poc/sunburst.py
+++ poc/sunburst.py
@@ -3,14 +3,9 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 import sys
+import json
 
 from math import sin, cos, radians, atan2, sqrt, degrees
-
-# Custom imports
-import cutevariant.commons as cm
-
-LOGGER = cm.logger()
-
 
 def lerp(p1: QPoint, p2: QPoint, alpha: float):
     return p1 * alpha + p2 * (1 - alpha)
@@ -22,82 +17,8 @@ def in_interval(x, a, b):
 
 class SunburstWidget(QWidget):
     """
-    Example json file:
-    {
-        "label" : "all",
-        "part" : 1,
-        "subparts" :
-        [
-            {
-                "label" : "HIGH"
-                "part" : 0.7,
-                "suparts" :
-                [
-                    {
-                        "label" : "not so HIGH",
-                        "part" : 0.4
-                    },
-                    {
-                        "label" : "mildly HIGH",
-                        "part" : 0.2
-                    },
-                    {
-                        "label" : "somehow HIGH",
-                        "part" : 0.1
-                    },
-                    {
-                        "label" : "pretty HIGH",
-                        "part" : 0.3
-                    }
-                ]
-            },
-            {
-                "label" : "LOW",
-                "part" : 0.3,
-                "subparts" :
-                [
-                    {
-                        "label" : "kinda LOW",
-                        "part" : 0.4
-                    },
-                    {
-                        "label" : "really LOW",
-                        "part" : 0.6
-                    }
-                ]
-            }
-        ]
-    }
-
-    New example file (simpler)
-    {
-        "HIGH" :
-        {
-            "part" : 0.7,
-            "subparts":
-            [
-                {"not so HIGH" : 0.4},
-                {"mildly HIGH" : 0.2},
-                {"somehow HIGH" : 0.1},
-                {"pretty HIGH" : 0.3}
-            ]
-        },
-        "LOW" :
-        {
-            "part" : 0.3,
-            "subparts":
-            [
-                {"kinda LOW" : 0.4},
-                {"really LOW" : 0.6}
-            ]
-        }
-    }
-    Out of this kind of json input, it shows each layer of the graph in a different color
-    """
-
-    current_label_changed = Signal(str)
-
-    EXAMPLE_DATA = [
+    Example data
+    [
         {
             "label": "HIGH",
             "part" : 0.7,
@@ -109,21 +30,21 @@ class SunburstWidget(QWidget):
                     "label" : "not so HIGH",
                     "part" : 0.4,
                     "color" : 0,
-                    # "subparts":
-                    # [
-                    #     {
-                    #         "parent" : "not so HIGH",
-                    #         "label" : "whatever",
-                    #         "part" : 0.5,
-                    #         "color" : 0
-                    #     },
-                    #     {
-                    #         "parent" : "not so HIGH",
-                    #         "label" : "other",
-                    #         "part" : 0.5,
-                    #         "color" : 0
-                    #     }
-                    # ]
+                    "subparts":
+                    [
+                        {
+                            "parent" : "not so HIGH",
+                            "label" : "whatever",
+                            "part" : 0.5,
+                            "color" : 0
+                        },
+                        {
+                            "parent" : "not so HIGH",
+                            "label" : "other",
+                            "part" : 0.5,
+                            "color" : 0
+                        }
+                    ]
                 },
                 {
                     "parent" : "HIGH",
@@ -184,6 +105,98 @@ class SunburstWidget(QWidget):
             ]
         }
     ]
+    Out of this kind of json input, it shows each layer of the graph in a different color
+    """
+
+    current_label_changed = Signal(str)
+
+    EXAMPLE_DATA = [
+        {
+            "label": "HIGH",
+            "part" : 0.7,
+            "color" : 0,
+            "subparts":
+            [
+                {
+                    "parent" : "HIGH",
+                    "label" : "not so HIGH",
+                    "part" : 0.4,
+                    "color" : 0,
+                    "subparts":
+                    [
+                        {
+                            "parent" : "not so HIGH",
+                            "label" : "whatever",
+                            "part" : 0.5,
+                            "color" : 0
+                        },
+                        {
+                            "parent" : "not so HIGH",
+                            "label" : "other",
+                            "part" : 0.5,
+                            "color" : 0
+                        }
+                    ]
+                },
+                {
+                    "parent" : "HIGH",
+                    "label" : "mildly HIGH",
+                    "part" : 0.2,
+                    "color" : 0
+                },
+                {
+                    "parent" : "HIGH",
+                    "label" : "somehow HIGH",
+                    "part" : 0.1,
+                    "color" : 0
+                },
+                {
+                    "parent" : "HIGH",
+                    "label" : "pretty HIGH",
+                    "part" : 0.3,
+                    "color" : 0
+                }
+            ]
+        },
+        {
+            "label" : "LOW",
+            "part" : 0.3,
+            "color" : 180,
+            "subparts":
+            [
+                {
+                    "parent" : "LOW",
+                    "label" : "kinda LOW",
+                    "part" : 0.4,
+                    "color" : 180,
+                    "subparts" :
+                    [
+                        {
+                            "parent" : "kinda LOW",
+                            "label" : "just a test",
+                            "part" : 0.2
+                        },
+                        {
+                            "parent" : "kinda LOW",
+                            "label" : "another test",
+                            "part" : 0.5,
+                        },
+                        {
+                            "parent" : "kinda LOW",
+                            "label" : "Now that's debugging \m/",
+                            "part" : 0.1
+                        }
+                    ]
+                },
+                {
+                    "parent" : "LOW",
+                    "label" : "really LOW",
+                    "part" : 0.6,
+                    "color" : 180
+                }
+            ]
+        }
+    ]
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -196,33 +209,34 @@ class SunburstWidget(QWidget):
         self._area = QRect(0,0,0,0)
 
         self.setMouseTracking(True)
-        # self.painter_paths = []
         self._painter_paths = {}
-        #self._max_depth = 10 # Above 10 rings, the data is not readable...
-
-        self.build_chart()
+        self._max_depth = 10 # Above 10 rings, the data is not readable...
 
     def set_data(self, data: dict):
         self._data = data
         self._visible_tree = data
         self.build_chart()
-        # self.arc_paths = []
         self.update()
 
-    def create_layer(self,rect: QRect,layer:list,depth:int,inner_most_radius:int,width:int):
+    def create_layer(self,layer:list,depth:int,inner_most_radius:int,width:int):
         """
         Adds a ring of width :width: after skipping depth rings from inner_most_radius
         :depth: ring index of this layer. 0 means it is the inner_most ring, displaying the data from the very first element of tree
         :tree: a dict of dicts, containing recursively all the nodes of the data to display. For each node, required keys are label and part. Optionnals include color and subparts
         :inner_most_radius: the interior radius of the very first ring
         """
+        if depth > self._max_depth: # No need to go further down in the recursion, max_depth has been reached !
+            return
+        
+        rect = self._area
         inner_radius = inner_most_radius + depth * width
         outer_radius = inner_radius + width
         start_angle = 0
         parent_name = None
         parent_part = 1.0 # Remember, each part is relative to the previous depth's part (absolute if depth is 0)
         
-        self._painter_paths[depth] = {}
+        if depth not in self._painter_paths: # Somehow hacky. Just to make sure there is a key for depth, for the rest of the function to fill
+            self._painter_paths[depth] = {}
 
         if depth > 0:
             parent_name = layer[0]["parent"]
@@ -254,12 +268,9 @@ class SunburstWidget(QWidget):
                                             "path" : SunburstWidget.create_arc_path(rect,inner_radius,outer_radius,start_angle,span_angle)
                                         }
 
-            # if label=="somehow HIGH":
-            #     print(start_angle,span_angle)
 
-            #print(f"Just drew {label} with angle from {start_angle} to {start_angle+span_angle}")
             if "subparts" in cell:
-                self.create_layer(rect,cell["subparts"],depth+1,inner_most_radius,width)
+                self.create_layer(cell["subparts"],depth+1,inner_most_radius,width)
                 #cell["subparts"]["parent"]=cell["label"]
                 
             
@@ -281,23 +292,7 @@ class SunburstWidget(QWidget):
         """
         side = min(self.rect().size().toTuple())
 
-        self.create_layer(self._area,self._visible_tree,0,side/8,side/8)
-
-
-        # r1 = side / 2
-        # r2 = r1 + side / 4
-
-        # start_angle = 0
-
-        # for angle in [0.5, 0.4, 0.1]:
-
-        #     painter_path = SunburstWidget.create_arc_path(
-        #         area, r1, r2, start_angle, angle * 360
-        #     )
-
-        #     self.painter_paths.append(painter_path)
-
-        #     start_angle += angle * 360
+        self.create_layer(self._visible_tree,0,side/8,side/8)
 
     def paintEvent(self, event: QPaintEvent):
 
@@ -310,56 +305,31 @@ class SunburstWidget(QWidget):
         for depth in self._painter_paths:# Draw every layer
             for label in self._painter_paths[depth]: # For each layer, loop through all of its labels
                 path = self._painter_paths[depth][label]["path"]
-                h,s,v = self._painter_paths[depth][label]["color"].values()
+                h,s,v = self._painter_paths[depth][label]["color"].values() # Not so good looking but works (because of recent python respect for insertion order in dicts)
                 color = QColor.fromHsv(h,s,v)
                 painter.fillPath(path,color)
 
-
-        # for i, path in enumerate(self.painter_paths):
-        #     painter.fillPath(path, QColor(i * 100, 255, 255))
-
-        # sat = 100
-
-        # color = QColor.fromHsv((start_angle * 4) % 100, sat, 255)
-
-        # # sat = 100
-        # # if in_interval(self._mouse_zone[1], start_angle, start_angle + 360 * angle):
-        # #     sat = 255
-        # # color = QColor.fromHsv((start_angle * 4) % 100, sat, 255)
-
-        # start_angle += angle * 360
-
-        # painter.fillPath(painter_path, color)
-        # painter.end()
-
     def mouseMoveEvent(self, event: QMouseEvent):
         side = min(self.rect().size().toTuple())
-
-        x,y = event.pos().toTuple()
-        xc,yc = self.rect().center().toTuple()
-
-        radius = sqrt((x-xc)**2+(y-yc)**2) # get the distance between the mouse and the center of the widget
 
         previously_selected_label = self._selected_label
 
         self._selected_label = "" # If the mouse moves, it means (potentially) that the previously selected label may not be anymore
 
-        #if radius >= side/8: # The mouse is not inside the center disc, so we need to know which ring/label was selected 
-        for depth in self._painter_paths:# Check at every layer
-            for label in self._painter_paths[depth]: # For each layer, loop through its labels
-                if self._painter_paths[depth][label]["path"].contains(event.pos()): # Find the painter path drawn at this depth, for this label
+        # The purpose of these loops is to find the painter path that the mouse hovers.
+        # To do so, we need to loop through every label of every layer (depth).
+        for depth in self._painter_paths:
+            for label in self._painter_paths[depth]:
+                if self._painter_paths[depth][label]["path"].contains(event.pos()): # The mouse is hovering this path...
                     self._selected_label = label # There we have it. The selected label !
-                    self._painter_paths[depth][label]["color"]["s"]=255
+                    self._painter_paths[depth][label]["color"]["s"]=255 # Set the saturation of the label to max to highlight it
                 else:
-                    self._painter_paths[depth][label]["color"]["s"]=100
+                    self._painter_paths[depth][label]["color"]["s"]=100 # This label is not selected, set its saturation to 100
         
-        self.update() # Update the graph, only if the mouse was not in the center. We update it even if the mouse doesn't touch any label (in that case, it will be desaturated)
+        self.update() # Update the graph. We update it even if the mouse doesn't touch any label (in that case, it will be desaturated)
 
         if self._selected_label != previously_selected_label:
             self.current_label_changed.emit(self._selected_label)
-        # x, y = event.localPos().x(), event.localPos().y()
-        # xc = self.geometry().width() / 2
-        # yc = self.geometry().height() / 2
 
     def create_arc_path(
         rect: QRect,
