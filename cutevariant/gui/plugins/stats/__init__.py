8a5d834a26a575f7bd893167c80a23601985f593
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-04-29 19:23:57 +0200

    [stats plugin] Added stats plugin, very experimental
    Causes segfault when selecting some fields that return error with numpy functions...

---
+++ cutevariant/gui/plugins/stats/__init__.py
@@ -0,0 +1,13 @@
+from PySide2.QtWidgets import QApplication
+
+__title__ = "Statistics"
+__description__ = "A plugin to display statistics for the selected "
+__long_description__ = QApplication.instance().translate(
+    "stats_widget",
+    """
+<p>This plugins shows general statistics about a selected column.</p>
+It can be used to evaluate
+""",
+)
+__author__ = "Charles Monod-Broca"
+__version__ = "1.0.0"
