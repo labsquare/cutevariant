eb468eb7a2662a98fe8111bdcdb27bd18aa135d1
sacha <sacha@labsquare.org>
2021-05-20 00:24:21 +0200

    fix #252

--- cutevariant/gui/formatters/cutestyle.py
+++ cutevariant/gui/formatters/cutestyle.py
@@ -30,9 +30,9 @@ class CutestyleFormatter(Formatter):
     ACMG_ICON = {
         "0": FIcon(0xF03A1, "lightgray"),
         "1": FIcon(0xF03A4, "#71e096"),
-        "2": FIcon(0xF03A7, "#71e096"),
+        "2": FIcon(0xF03A7, "#a7ecbe"),
         "3": FIcon(0xF03AA, "#f5a26f"),
-        "4": FIcon(0xF03AD, "#ed6d79"),
+        "4": FIcon(0xF03AD, "#f9cdd1"),
         "5": FIcon(0xF03B1, "#ed6d79"),
     }
 
