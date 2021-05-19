2b02b4706d1c85102929a55bcf8980ab82b2d446
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-05-19 16:57:09 +0200

    [harmonizome poc] Implemented dialog to actually add selected genes to a new wordset

--- poc/harmonizome.py
+++ poc/harmonizome.py
@@ -1,3 +1,4 @@
+import sqlite3
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -6,6 +7,11 @@ import sys
 from urllib.request import urlopen
 import json
 import re
+import os
+import tempfile
+
+from cutevariant.core.sql import get_sql_connection, get_wordsets
+from cutevariant.core.command import import_cmd
 
 URL_PREFIX = "https://maayanlab.cloud/Harmonizome"
 VERSION = "/api/1.0"
@@ -450,26 +456,127 @@ class HarmonizomeWidget(QWidget):
         self.selected_genes = self.selected_genes.difference(selected_genes)
         self.selected_genes_model.setStringList(self.selected_genes)
 
+    def get_selected_genes(self):
+        return list(self.selected_genes)
+
 
 class HarmonizomeDialog(QDialog):
-    def __init__(self, parent: QWidget = None) -> None:
+    def __init__(self, conn: sqlite3.Connection = None, parent: QWidget = None) -> None:
         super().__init__(parent)
         self.harmonizome_widget = HarmonizomeWidget(self)
-        self.buttons = QDialogButtonBox(
-            QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self
-        )
-        self.buttons.accepted.connect(self.accept)
-        self.buttons.rejected.connect(self.reject)
+        self.add_wordset_btn = QPushButton(self.tr("Create wordset"), self)
+        self.cancel_btn = QPushButton(self.tr("Cancel"), self)
+        self.cancel_btn.setDefault(True)
 
-        layout = QGridLayout(self)
-        layout.addWidget(self.harmonizome_widget, 0, 0)
-        layout.addWidget(self.buttons, 1, 0)
+        self.cancel_btn.clicked.connect(self.reject)
+        self.add_wordset_btn.clicked.connect(self.add_wordset)
+
+        self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addItem(
+            QSpacerItem(30, 0, QSizePolicy.Expanding, QSizePolicy.Fixed)
+        )
+        self.buttons_layout.addWidget(self.cancel_btn)
+        self.buttons_layout.addWidget(self.add_wordset_btn)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.harmonizome_widget)
+        layout.addLayout(self.buttons_layout)
+
+        self.set_names = []
+
+        self.conn = conn
+
+    @property
+    def conn(self):
+        return self._conn
+
+    @conn.setter
+    def conn(self, value: sqlite3.Connection):
+        self._conn = value
+        # We just changed conn, so the set_names are not relevant anymore
+        self.set_names.clear()
+        if self._conn:
+            self.set_names = [data["name"] for data in get_wordsets(self._conn)]
+
+    def import_wordset(self, words, wordset_name):
+        """Import given words into a new wordset in database
+
+        Warnings:
+            There is NO CHECK on manual user's inputs! Except during DB insertion.
+
+        Args:
+            words(list): List of words to be inserted
+            wordset_name(str): Name of the word set
+
+        Note:
+            Copy pasted from word_set plugin... Code replication is bad !
+
+        Returns:
+            (boolean): Status of the wordset creation
+        """
+
+        if self.conn:
+            # Dump the list in a temporary file
+            fd, filename = tempfile.mkstemp()
+            with open(filename, "w") as file:
+                [file.write(word + "\n") for word in words]
+
+            # Import the content of the temp file in DB
+            result = import_cmd(self.conn, "wordsets", wordset_name, filename)
+
+            if not result["success"]:
+                QMessageBox.critical(
+                    self,
+                    self.tr("Error while importing set"),
+                    self.tr("Error while importing set '%s'") % wordset_name,
+                )
+
+            os.close(fd)
+            os.remove(filename)
+            return result["success"]
+
+    def add_wordset(self):
+        """Display a window to allow to add/edit/remove word sets
+
+        The set is then imported in database.
+        """
+
+        wordset_name = None
+        while not wordset_name:
+            wordset_name, _ = QInputDialog.getText(
+                self, self.tr("Create a new set"), self.tr("Name of the new set:")
+            )
+            if not wordset_name:
+                return
+
+            if wordset_name in self.set_names:
+                # Name already used
+                QMessageBox.critical(
+                    self,
+                    self.tr("Error while creating set"),
+                    self.tr("Error while creating set '%s'; Name is already used")
+                    % wordset_name,
+                )
+                wordset_name = None
+
+        # Import and close dialog
+        if self.import_wordset(
+            self.harmonizome_widget.get_selected_genes(), wordset_name
+        ):
+            QMessageBox.information(
+                self,
+                self.tr("Success!"),
+                self.tr(f"Successfully imported wordset {wordset_name}"),
+            )
+        self.accept()
 
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    w = HarmonizomeDialog()
+    w = HarmonizomeDialog(
+        get_sql_connection("/home/charles/bioinfo/cutevariant_projects/example.db")
+    )
     w.show()
     app.exec_()
