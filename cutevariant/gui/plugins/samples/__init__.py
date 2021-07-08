30103959115f5b9e9e7c86bf69975f9599a9aa02
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-07-08 15:04:01 +0200

    [samples view] Updated sampels view plugin

---
+++ cutevariant/gui/plugins/samples/__init__.py
@@ -0,0 +1,12 @@
+from PySide2.QtWidgets import QApplication
+
+__title__ = "Samples view"
+__description__ = "A plugin to display all FORMAT fields (sample-wise information) from the current variant"
+__long_description__ = QApplication.instance().translate(
+    "Samples view",
+    """
+    Display all sample information available from the currently selected variant. 
+""",
+)
+__author__ = "Sacha schutz"
+__version__ = "1.0.0"
