51d2fa868ca85ee57217943e4b7ca92f57828bdb
sacha <sacha@labsquare.org>
2021-07-09 23:22:44 +0200

    Rewrite commit more simple

--- cutevariant/config.py
+++ cutevariant/config.py
@@ -1,154 +1,101 @@
 from collections import ChainMap
 import os
 import yaml
+import typing
 from PySide2.QtCore import QStandardPaths, QDir, QFile, QFileInfo
 
 
 class Config:
-    def __init__(self, root="global", user_config_path=None):
 
-        self.user_config_path = user_config_path or QDir(
+    """Config class help you to read and write config variables
+
+    Attributes:
+        config_path (str): a path to a config file using yaml format
+        default_config_path(str, readonly): path to the default config path
+        section (str): the key section to use.
+
+    Examples:
+        config = Config("app")
+        config["memory"] = 10
+        # Save into the default location ~/.config/cutevariant/config.yaml
+        config.save()
+
+        config = Config("plugin_name")
+        max_row = config["max_row"]
+
+        # Reset default config
+        config.reset()
+
+    """
+
+    def __init__(self, section="app", config_path=None, load=True):
+        self.section = section
+        self.config_path = config_path or QDir(
             QStandardPaths.writableLocation(QStandardPaths.ConfigLocation)
             + QDir.separator()
             + "cutevariant"
         ).absoluteFilePath("config.yml")
 
-        self.plugins_path = (
-            os.path.dirname(__file__)
-            + QDir.separator()
-            + "gui"
-            + QDir.separator()
-            + "plugins"
-        )
+        self._user_config = dict()
 
-        # Either 'global' or the name of a plugin
-        self.root = root
-
-        self.default_config = {}
-        self.user_config = {}
-
-        self.load()
+        if load:
+            self.load()
 
     @property
-    def root(self):
-        """return config root"""
-        return self._root
+    def default_config_path(self):
+        return os.path.dirname(__file__) + os.path.sep + "default_config.yml"
 
-    @root.setter
-    def root(self, value: str):
-        # TODO : check if value is global or plugins
-        self._root = value
+    def get(self, key: str, default=None):
 
-    def _create_user_config(self, remove=True):
-        """create user config directory"""
-        if not os.path.exists(os.path.dirname(self.user_config_path)):
-            try:
-                os.makedirs(os.path.dirname(self.user_config_path))
-            except OSError as exc:  # Guard against race condition
-                if exc.errno != errno.EEXIST:
-                    raise
+        if self.section not in self._user_config:
+            self._user_config[self.section] = {}
 
-        self.reset()
-        self.save()
+        return self._user_config[self.section].get(key, default)
+
+    def set(self, key: str, value: typing.Any):
+
+        if self.section not in self._user_config:
+            self._user_config[self.section] = {}
+
+        self._user_config[self.section][key] = value
 
     def load(self):
 
-        # Load default config
-        self.default_config = {}
+        if not os.path.exists(self.config_path):
+            self.save()
 
-        ## Load global config
-        with open(os.path.dirname(__file__) + os.path.sep + "config.yml", "r") as file:
+        with open(self.config_path, "r") as stream:
             try:
-                self.default_config["global"] = yaml.safe_load(file)
+                self._user_config = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
-                print("cannot read default global config")
-
-        ## Load plugins config
-        self.default_config["plugins"] = {}
-        for plugin in QDir(self.plugins_path).entryInfoList(
-            QDir.Dirs | QDir.NoDotDot | QDir.NoDot
-        ):
-            config_file = QDir(plugin.absoluteFilePath()).absoluteFilePath("config.yml")
-            # TODO : test if it is a module
-            plugin_name = plugin.baseName()
-            if os.path.exists(config_file):
-                with open(config_file, "r") as file:
-                    try:
-                        self.default_config["plugins"][plugin_name] = yaml.safe_load(
-                            file
-                        )
-                    except yaml.YAMLError as exc:
-                        print(f"cannot read plugin {plugin_name} config")
-            else:
-                self.default_config["plugins"][plugin_name] = {}
-
-        # Load user config
-        self.user_config = {}
-
-        """ create user config if not exists """
-        if not os.path.exists(self.user_config_path):
-            self._create_user_config()
-
-        with open(self.user_config_path, "r") as file:
-            try:
-                self.user_config = yaml.safe_load(file)
-            except yaml.YAMLError as exc:
-                print("cannot read user config")
+                print(exc)
+            except KeyError as err:
+                print(f"cannot read section {self.section} from config ")
 
     def save(self):
-        with open(self.user_config_path, "w") as file:
-            yaml.dump(self.user_config, file)
+        with open(self.config_path, "w") as stream:
+            yaml.dump(self._user_config, stream)
 
     def reset(self):
-        self.user_config = self.default_config
+        previous_path = self.config_path
+        self.config_path = self.default_config_path
+        self.load()
+        self.config_path = previous_path
+        self.save()
 
-    def __getitem__(self, key):
-        """Return user config if exists. Otherwise, returns default settings"""
+    def __getitem__(self, key: str):
+        return self._user_config[self.section][key]
 
-        # It is a global
-        if self.root == "global":
-            if self.root in self.user_config:
-                return ChainMap(
-                    self.user_config[self.root], self.default_config[self.root]
-                )[key]
+    def __setitem__(self, key: str, value: typing.Any):
+        self.set(key, value)
 
-            else:
-                return self.default_config[self.root][key]
-
-        # It is a plugins
-        elif self.root in self.default_config["plugins"]:
-            if self.root in self.user_config.get("plugins", ()):
-                return ChainMap(
-                    self.user_config["plugins"][self.root],
-                    self.default_config["plugins"][self.root],
-                )[key]
-            else:
-                return self.default_plugin_config["plugins"][self.root][key]
-
-    def __setitem__(self, key, value):
-        """Set user config"""
-
-        # check if root is a valid root by comparing with default config
-        if self.root not in ["global"] + list(self.default_config["plugins"].keys()):
-            return
-
-        if self.root not in self.user_config:
-            self.user_config[self.root] = {}
-
-        if self.root == "global":
-            self.user_config[self.root][key] = value
-        else:
-            self.user_config[self.root]["plugins"][key] = value
+    def __contains__(self, key):
+        return key in self._user_config[self.section]
 
 
 if __name__ == "__main__":
 
     config = Config("global")
-    config["style"] = "blue"
+    config.load()
+
     print(config["style"])
-
-    config.root = "variant_view"
-    config["memory_cache"] = 100
-    print("dd", config["memory_cache"])
-
-    config.save()
--- tests/test_config.py
+++ tests/test_config.py
@@ -7,24 +7,33 @@ import yaml
 from cutevariant.config import Config
 
 
-@pytest.fixture
-def config():
-    c = Config(user_config_path=tempfile.mktemp())
-    assert "global" in c.default_config
-    return c
+def test_config():
+    config_path = tempfile.mktemp()
+    config = Config("app", config_path)
 
+    # # Test WRITE
+    config["memory"] = 10
+    config["network.host"] = "192.168.1.1"
 
-def test_config(config: Config):
-    assert os.path.exists(config.user_config_path)
+    # Test IN
+    assert "memory" in config
 
-    config.default_config["global"]["test"] = "sacha"
-    assert config["test"] == "sacha"
-
-    config["test2"] = "charles"
-    assert config["test2"] == "charles"
-
+    # Test save
     config.save()
+    assert os.path.exists(config_path)
+    print(config_path)
 
-    with open(config.user_config_path, "r") as f:
-        config_data = yaml.safe_load(f)
-        assert config_data["global"]["test2"] == "charles"
+    with open(config_path) as file:
+        data = yaml.safe_load(file)
+        assert data["app"]["memory"] == 10
+        assert data["app"]["network.host"] == "192.168.1.1"
+
+    # Test READ
+    config = Config("app", config_path)
+    assert config["memory"] == 10
+    # Test if default config exist
+    assert os.path.exists(config.default_config_path)
+
+    # Test reset path
+    config.reset()
+    assert config["style"] == "bright"
