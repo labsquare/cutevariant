ca0dab8c869a877b108096330870b1c0387c92c0
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-07-18 19:15:20 +0200

    [developers] Now template generation works
    Only needs a few improvements so that the resulting python code is
    completely working (#277)

--- cutevariant/gui/plugin_form.py
+++ cutevariant/gui/plugin_form.py
@@ -1,3 +1,4 @@
+from cutevariant.gui import plugin
 import jinja2
 import os
 import re
@@ -9,26 +10,10 @@ from PySide2.QtWidgets import *
 from cutevariant.commons import camel_to_snake, snake_to_camel
 from cutevariant import LOGGER
 
-PLUGIN_TEMPLATE = "plugin_template.txt"
-DIALOG_PLUGIN_TEMPLATE = "plugin_dialog_template.txt"
+WIDGET_TEMPLATE = "plugin_template.txt"
+DIALOG_TEMPLATE = "plugin_dialog_template.txt"
 INIT_TEMPLATE = "init_template.txt"
-
-
-def clean_human_text(words: str) -> str:
-    """Returns clean, space separated words from input string
-    with punctuation.
-
-    Example:
-        >>> clean_human_text("My plugin, with commas and exclamation point!")
-        'My plugin with commas and exclamation point'
-
-    Args:
-        words (str): String with words and punctuation
-
-    Returns:
-        str: String with only words, separated by space
-    """
-    return re.sub(r"\W+", " ", words).strip()
+SETTINGS_TEMPLATE = "plugin_settings_template.txt"
 
 
 def human_to_camel(words: str) -> str:
@@ -48,106 +33,138 @@ def human_to_camel(words: str) -> str:
     return "".join([w.capitalize() for w in words.split()])
 
 
-def generate_file(file_type: str, **kwargs):
+def generate_file(template_file: str, name: str, **kwargs):
     """Writes all the files required for a plugin to work.
     This function should be called twice per plugin, once for the plugin itself, and once for the __init__.py file.
 
     Args:
         file_type (str): One of PLUGIN_TEMPLATE, DIALOG_TEMPLATE, or INIT_TEMPLATE
     """
-    template_loader = jinja2.FileSystemLoader(searchpath="./")
-    TEMPLATE_FILE = file_type
-    if not os.path.isfile(f"./{TEMPLATE_FILE}"):
-        LOGGER.debug("Template %s not found!", TEMPLATE_FILE)
+    template_path = os.path.abspath(os.path.join(f"{os.path.dirname(__file__)}/../"))
+    template_loader = jinja2.FileSystemLoader(searchpath=template_path)
+
+    template_to_file_name = {
+        WIDGET_TEMPLATE: "widgets.py",
+        DIALOG_TEMPLATE: "dialogs.py",
+        SETTINGS_TEMPLATE: "settings.py",
+        INIT_TEMPLATE: "__init__.py",
+    }
+
+    if not os.path.isfile(os.path.join(template_path, template_file)):
+        LOGGER.debug("Template %s not found!", template_file)
         return
     template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(TEMPLATE_FILE)
+    template_env.filters["camel_to_snake"] = camel_to_snake
+    template = template_env.get_template(template_file)
     plugin_path = os.path.join(os.path.dirname(__file__), "plugins")
-    file_destination = os.path.join(plugin_path, camel_to_snake(kwargs["name"]))
+    plugin_path = os.path.join(plugin_path, camel_to_snake(name))
+    if not os.path.isdir(plugin_path):
+        os.mkdir(plugin_path)
+    file_destination = os.path.join(
+        plugin_path, template_to_file_name.get(template_file, "error.py")
+    )
     try:
-        output_text = template.render(**kwargs)
+        output_text = template.render(**kwargs, name=name)
         if output_text:
             with open(file_destination, "w") as file:
                 file.write(output_text)
     except Exception as e:
-        LOGGER.error("%s\n%s", e.args)
-
-
-def create_dialog_plugin(**kwargs):
-    pass
-
-
-def create_plugin(**kwargs):
-
-    template_loader = jinja2.FileSystemLoader(searchpath="./")
-    TEMPLATE_FILE = "plugin_template.txt"
-    if not os.path.isfile(f"./{TEMPLATE_FILE}"):
-        return
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(TEMPLATE_FILE)
-    plugin_path = os.path.join(os.path.dirname(__file__), "plugins")
-    os.path.join(plugin_path, kwargs["name"])
-    output_text = template.render(**kwargs)
-
-
-def create_dialog_plugin(**kwargs):
-    template_loader = jinja2.FileSystemLoader(searchpath="./")
-    TEMPLATE_FILE = "plugin_dialog_template.txt"
-    if not os.path.isfile(f"./{TEMPLATE_FILE}"):
-        return
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(TEMPLATE_FILE)
-    plugin_path = os.path.join(os.path.dirname(__file__), "plugins")
-    output_text = template.render(**kwargs)
-
-
-def create_init_file(**kwargs):
-    template_loader = jinja2.FileSystemLoader(searchpath="./")
-    TEMPLATE_FILE = "init_template.txt"
-    if not os.path.isfile(f"./{TEMPLATE_FILE}"):
-        return
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(TEMPLATE_FILE)
-    plugin_path = os.path.join(os.path.dirname(__file__), "plugins")
-    output_text = template.render(**kwargs)
+        LOGGER.error(e, exc_info=True)
 
 
 class PluginCreator(QDialog):
-    def __init__(self, parent: QWidget) -> None:
+    def __init__(self, parent: QWidget = None) -> None:
         super().__init__(parent=parent)
 
         vlayout = QVBoxLayout(self)
         self.form_layout = QFormLayout()
+
         self.le_name = QLineEdit()
+        self.name_validator = QRegularExpressionValidator()
+        # Allow letters only
+        self.name_validator.setRegularExpression(QRegularExpression(r"([a-zA-Z ])+"))
+        self.le_name.setValidator(self.name_validator)
+
+        self.label_resulting_name = QLabel("")
+        self.le_name.textChanged.connect(
+            lambda s: self.label_resulting_name.setText(f"{human_to_camel(s)}Plugin")
+        )
+
+        self.label_resulting_module = QLabel("")
+        self.le_name.textChanged.connect(
+            lambda s: self.label_resulting_module.setText(
+                camel_to_snake(human_to_camel(s))
+            )
+        )
+
         self.le_title = QLineEdit()
         self.le_description = QLineEdit()
-        self.le_long_description = QLineEdit()
+        self.te_long_description = QTextEdit()
         self.le_author = QLineEdit()
-        self.le_version = QLineEdit()
 
-        self.buttons = QDialogButtonBox(self)
-        self.buttons.button(QDialogButtonBox.Ok).clicked.connect(self.accept)
-        self.buttons.button(QDialogButtonBox.Cancel).clicked.connect(self.reject)
+        self.frame_plugin_type = QFrame(self)
+        frame_layout = QVBoxLayout(self.frame_plugin_type)
+        self.check_add_dialog = QCheckBox(self.tr("Dialog plugin"), self)
+        self.check_add_widget = QCheckBox(self.tr("Widget plugin"), self)
+        self.check_add_settings = QCheckBox(self.tr("Add plugin settings"), self)
+        frame_layout.addWidget(self.check_add_dialog)
+        frame_layout.addWidget(self.check_add_widget)
+        frame_layout.addWidget(self.check_add_settings)
 
-        self.form_layout.addRow(self.tr("Plugin name"), self.le_name)
+        self.dialog_box = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        )
+        self.dialog_box.button(QDialogButtonBox.Ok).clicked.connect(self.accept)
+        self.dialog_box.button(QDialogButtonBox.Cancel).clicked.connect(self.reject)
+
+        self.form_layout.addRow(self.tr("Plugin name (space separated)"), self.le_name)
+        self.form_layout.addRow(
+            self.tr("Resulting class name:"), self.label_resulting_name
+        )
+        self.form_layout.addRow(
+            self.tr("Resulting module name:"), self.label_resulting_module
+        )
         self.form_layout.addRow(self.tr("Plugin title"), self.le_title)
         self.form_layout.addRow(self.tr("Description"), self.le_description)
-        self.form_layout.addRow(self.tr("Long description"), self.le_long_description)
+        self.form_layout.addRow(self.tr("Long description"), self.te_long_description)
         self.form_layout.addRow(self.tr("Author"), self.le_author)
-        self.form_layout.addRow(self.tr("Version"), self.le_version)
+        self.form_layout.addRow(self.tr("Plugin type"), self.frame_plugin_type)
 
         vlayout.addLayout(self.form_layout)
-        vlayout.addWidget(self.buttons)
+        vlayout.addWidget(self.dialog_box)
 
         self.plugin_info = {}
 
     def accept(self) -> None:
         self.plugin_info = {
-            "name": human_to_camel(clean_human_text(self.le_name.text())),
+            "name": human_to_camel(self.le_name.text()),
             "title": self.le_title.text(),
             "description": self.le_description.text(),
-            "long_description": self.le_long_description.text(),
+            "long_description": self.te_long_description.toPlainText(),
             "author": self.le_author.text(),
-            "version": self.le_version.text(),
+            "add_settings": self.check_add_settings.isChecked(),
+            "add_widget": self.check_add_widget.isChecked(),
+            "add_dialog": self.check_add_dialog.isChecked(),
         }
         return super().accept()
+
+
+def create_dialog_plugin():
+    dialog = PluginCreator()
+    if dialog.exec_() == QDialog.Accepted:
+        plugin_info = dialog.plugin_info
+        if "add_settings" in plugin_info and plugin_info["add_settings"]:
+            generate_file(SETTINGS_TEMPLATE, **plugin_info)
+        if "add_widget" in plugin_info and plugin_info["add_widget"]:
+            generate_file(WIDGET_TEMPLATE, **plugin_info)
+        if "add_dialog" in plugin_info and plugin_info["add_dialog"]:
+            generate_file(DIALOG_TEMPLATE, **plugin_info)
+
+        generate_file(INIT_TEMPLATE, **plugin_info)  # Mandatory!
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    create_dialog_plugin()
