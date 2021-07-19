91da9364f169a175e24e7655c6675f4a5779ae17
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-07-19 13:02:23 +0200

    [plugin generator] Refactor and ui improvements
    Refactor simplifies the code, making it safier as well.
    UI is now looking a bit better

--- cutevariant/gui/plugin_form.py
+++ cutevariant/gui/plugin_form.py
@@ -33,16 +33,15 @@ def human_to_camel(words: str) -> str:
     return "".join([w.capitalize() for w in words.split()])
 
 
-def generate_file(template_file: str, name: str, **kwargs):
+def generate_file(template_file: str, **kwargs):
     """Writes all the files required for a plugin to work.
     This function should be called twice per plugin, once for the plugin itself, and once for the __init__.py file.
 
     Args:
         file_type (str): One of PLUGIN_TEMPLATE, DIALOG_TEMPLATE, or INIT_TEMPLATE
     """
-    template_path = os.path.abspath(os.path.join(f"{os.path.dirname(__file__)}/../"))
-    template_loader = jinja2.FileSystemLoader(searchpath=template_path)
 
+    # Maps a template type to its resulting file name once rendered
     template_to_file_name = {
         WIDGET_TEMPLATE: "widgets.py",
         DIALOG_TEMPLATE: "dialogs.py",
@@ -50,21 +49,40 @@ def generate_file(template_file: str, name: str, **kwargs):
         INIT_TEMPLATE: "__init__.py",
     }
 
+    # name (the plugin name in camelCase) is required. Should be something like: MySample
+    if "name" not in kwargs:
+        return
+
+    # module name (the plugin name in snake_case) is required. Should be something like: my_sample
+    if "module_name" not in kwargs:
+        return
+
+    # So that we can safely map a template name to its end target
+    if template_file not in template_to_file_name.keys():
+        return
+
+    name = kwargs["name"]
+    module_name = kwargs["module_name"]
+
+    template_path = os.path.abspath(os.path.join(f"{os.path.dirname(__file__)}/../"))
+    template_loader = jinja2.FileSystemLoader(searchpath=template_path)
+
     if not os.path.isfile(os.path.join(template_path, template_file)):
         LOGGER.debug("Template %s not found!", template_file)
         return
     template_env = jinja2.Environment(loader=template_loader)
     template_env.filters["camel_to_snake"] = camel_to_snake
     template = template_env.get_template(template_file)
-    plugin_path = os.path.join(os.path.dirname(__file__), "plugins")
-    plugin_path = os.path.join(plugin_path, camel_to_snake(name))
+
+    # Find path to this plugin
+    plugin_path = os.path.join(os.path.dirname(__file__), "plugins", module_name)
+
     if not os.path.isdir(plugin_path):
         os.mkdir(plugin_path)
-    file_destination = os.path.join(
-        plugin_path, template_to_file_name.get(template_file, "error.py")
-    )
+    file_destination = os.path.join(plugin_path, template_to_file_name[template_file])
+
     try:
-        output_text = template.render(**kwargs, name=name)
+        output_text = template.render(**kwargs)
         if output_text:
             with open(file_destination, "w") as file:
                 file.write(output_text)
@@ -85,19 +103,20 @@ class PluginCreator(QDialog):
         self.name_validator.setRegularExpression(QRegularExpression(r"([a-zA-Z ])+"))
         self.le_name.setValidator(self.name_validator)
 
-        self.label_resulting_name = QLabel("")
+        self.le_resulting_name = QLineEdit("")
+        self.le_resulting_name.setReadOnly(True)
         self.le_name.textChanged.connect(
-            lambda s: self.label_resulting_name.setText(f"{human_to_camel(s)}Plugin")
+            lambda s: self.le_resulting_name.setText(f"{human_to_camel(s)}Plugin")
         )
 
-        self.label_resulting_module = QLabel("")
+        self.le_resulting_module = QLineEdit("")
+        self.le_resulting_module.setReadOnly(True)
         self.le_name.textChanged.connect(
-            lambda s: self.label_resulting_module.setText(
+            lambda s: self.le_resulting_module.setText(
                 camel_to_snake(human_to_camel(s))
             )
         )
 
-        self.le_title = QLineEdit()
         self.le_description = QLineEdit()
         self.te_long_description = QTextEdit()
         self.le_author = QLineEdit()
@@ -117,28 +136,38 @@ class PluginCreator(QDialog):
         self.dialog_box.button(QDialogButtonBox.Ok).clicked.connect(self.accept)
         self.dialog_box.button(QDialogButtonBox.Cancel).clicked.connect(self.reject)
 
-        self.form_layout.addRow(self.tr("Plugin name (space separated)"), self.le_name)
-        self.form_layout.addRow(
-            self.tr("Resulting class name:"), self.label_resulting_name
-        )
-        self.form_layout.addRow(
-            self.tr("Resulting module name:"), self.label_resulting_module
-        )
-        self.form_layout.addRow(self.tr("Plugin title"), self.le_title)
-        self.form_layout.addRow(self.tr("Description"), self.le_description)
-        self.form_layout.addRow(self.tr("Long description"), self.te_long_description)
-        self.form_layout.addRow(self.tr("Author"), self.le_author)
-        self.form_layout.addRow(self.tr("Plugin type"), self.frame_plugin_type)
+        self.form_layout.addRow(self.tr("Name (space separated):"), self.le_name)
+        self.form_layout.addRow(self.tr("Class name:"), self.le_resulting_name)
+        self.form_layout.addRow(self.tr("Module name:"), self.le_resulting_module)
+        self.form_layout.addRow(self.tr("Description:"), self.le_description)
+        self.form_layout.addRow(self.tr("Long description:"), self.te_long_description)
+        self.form_layout.addRow(self.tr("Author:"), self.le_author)
+        self.form_layout.addRow(self.tr("Type:"), self.frame_plugin_type)
 
         vlayout.addLayout(self.form_layout)
         vlayout.addWidget(self.dialog_box)
 
         self.plugin_info = {}
 
+        self.resize(680, 480)
+
     def accept(self) -> None:
+        """Sets the result dictionnary, with all the format fields needed in the plugin templates.
+        name: plugin name, in camelCase. Example: MySample (note there is no 'Plugin' suffix)
+        title: same as plugin name, but space separated. Example: My sample
+        module_name: same as plugin name, but in snake_case. Example: my_sample
+        description: a single-line string
+        long_description: a longer description string, that can contain some line returns
+        author: a string
+        add_settings: Whether to generate a settings.py module
+        add_widget: Whether to generate a widgets.py module
+        add_dialog: Whether to generate a dialogs.py module
+
+        """
         self.plugin_info = {
             "name": human_to_camel(self.le_name.text()),
-            "title": self.le_title.text(),
+            "module_name": camel_to_snake(human_to_camel(self.le_name.text())),
+            "title": self.le_name.text(),
             "description": self.le_description.text(),
             "long_description": self.te_long_description.toPlainText(),
             "author": self.le_author.text(),
