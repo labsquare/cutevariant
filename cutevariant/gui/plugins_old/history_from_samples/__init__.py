69f6cbfdfc3a0d2fdb9bf7e6fc1a8605e3435474
SamuelNicaise <ichtyornis@gmail.com>
2022-04-19 11:31:28 +0200

    remove sql.py copies and plugins_old dir

--- cutevariant/gui/plugins_old/history_from_samples/__init__.py
+++
@@ -1,6 +0,0 @@
-__title__ = "History"
-__description__ = "The history of tables"
-__long_description__ = """This widget is directly linked to the variants, samples and sample_has_variant tables. 
-It displays, for each table history, the changes of entries."""
-__author__ = "Antony Le Béchec"
-__version__ = "1.0.0"
