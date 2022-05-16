a4da1a5800e4165a2ddc7ade41f28367f53048e2
Sacha Schutz <sacha@labsquare.org>
2022-05-16 20:33:53 +0200

    Add missing init

---
+++ cutevariant/gui/plugins/genotypes/__init__.py
@@ -0,0 +1,7 @@
+__title__ = "Genotype"
+__description__ = "A plugin to display genotype fields  "
+__long_description__ = """
+Docstring todo 
+"""
+__author__ = "Sacha schutz"
+__version__ = "1.0.0"
