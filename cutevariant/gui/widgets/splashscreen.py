1ca93ba90cd875fed861d7c3a6a039bfa0fc9606
Sacha Schutz <sacha@labsquare.org>
2022-06-22 16:48:44 +0200

    Update splashScreen

---
+++ cutevariant/gui/widgets/splashscreen.py
@@ -0,0 +1,26 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+
+from cutevariant import constants as cst
+
+
+class SplashScreen(QSplashScreen):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setPixmap(QPixmap(cst.DIR_ICONS + "splash.png"))
+
+    def paintEvent(self, event: QPaintEvent):
+        super().paintEvent(event)
+
+        painter = QPainter(self)
+
+        message = "Chargement ... "
+        rect = painter.fontMetrics().boundingRect(message)
+        rect.moveCenter(QPoint(self.rect().center().x(), self.rect().center().y() + 60))
+        painter.drawText(rect, Qt.AlignCenter, message)
+
+        message = f"Version {QApplication.applicationVersion()}"
+        rect = painter.fontMetrics().boundingRect(message)
+        rect.moveBottomRight(self.rect().bottomRight() - QPoint(30, 30))
+        painter.drawText(rect, Qt.AlignCenter, message)
