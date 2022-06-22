56bb926ef0b3e7c24a48f0f0f18c8a016eab0ae6
SamuelNicaise <ichtyornis@gmail.com>
2022-06-22 17:25:20 +0200

    sample selection in export

--- cutevariant/gui/export.py
+++ cutevariant/gui/export.py
@@ -37,6 +37,7 @@ class ExportDialog(QDialog):
         fields=["chr", "pos", "ref", "alt"],
         source="variants",
         filters={},
+        selected_samples=None,
         parent=None,
     ):
         super().__init__(parent)
@@ -44,6 +45,7 @@ class ExportDialog(QDialog):
         self.fields = fields
         self.source = source
         self.filters = filters
+        self.selected_samples = selected_samples
         self.conn = conn
 
         self.button_box = QDialogButtonBox(QDialogButtonBox.Cancel | QDialogButtonBox.Save)
@@ -105,8 +107,8 @@ class ExportDialog(QDialog):
 class BedExportDialog(ExportDialog):
     """Dialog to export database to a bed file"""
 
-    def __init__(self, conn, filename, fields, source, filters, parent=None):
-        super().__init__(conn, filename, fields, source, filters, parent)
+    def __init__(self, conn, filename, fields, source, filters, selected_samples, parent=None):
+        super().__init__(conn, filename, fields, source, filters, selected_samples, parent)
         self.set_central_widget(
             QLabel(self.tr("Will export BED file (tab-separated file with CHR, START, END)"))
         )
@@ -131,9 +133,10 @@ class CsvExportDialog(ExportDialog):
         fields,
         source,
         filters,
+        selected_samples,
         parent=None,
     ):
-        super().__init__(conn, filename, fields, source, filters, parent)
+        super().__init__(conn, filename, fields, source, filters, selected_samples, parent)
 
         form_layout = QFormLayout()
         self.combo = QComboBox()
@@ -181,8 +184,8 @@ class CsvExportDialog(ExportDialog):
 class PedExportDialog(ExportDialog):
     """Dialog to export database to a bed file"""
 
-    def __init__(self, conn, filename, fields, source, filters, parent=None):
-        super().__init__(conn, filename, fields, source, filters, parent)
+    def __init__(self, conn, filename, fields, source, filters, selected_samples, parent=None):
+        super().__init__(conn, filename, fields, source, filters, selected_samples, parent)
 
     def save(self):
         writer = PedWriter(self.conn, self.filename)
@@ -206,9 +209,10 @@ class VcfExportDialog(ExportDialog):
         fields,
         source,
         filters,
+        selected_samples,
         parent=None,
     ):
-        super().__init__(conn, filename, fields, source, filters, parent)
+        super().__init__(conn, filename, fields, source, filters, selected_samples, parent)
 
         self.group_box = QGroupBox()
         self.group_box.setTitle(self.tr("The following fields will be exported"))
