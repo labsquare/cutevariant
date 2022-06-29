e4288911a696e704ffe246383b49d1fda352231f
SamuelNicaise <ichtyornis@gmail.com>
2022-06-29 18:38:11 +0200

    interface

--- cutevariant/gui/plugins/compound_htz/dialogs.py
+++ cutevariant/gui/plugins/compound_htz/dialogs.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import *
 
 from cutevariant.gui.plugin import PluginDialog
 from cutevariant.gui import MainWindow
+from cutevariant.gui.plugins import compound_htz
 
 class CompoundHtzDialog(PluginDialog):
     """Model class for all tool menu plugins
@@ -25,17 +26,73 @@ class CompoundHtzDialog(PluginDialog):
 
         self.conn = conn
 
-        self.vlayout = QVBoxLayout()
-        self.button = QPushButton("hello")
-        self.button.clicked.connect(self.on_click)
+        group_box = QGroupBox()
+        header = QLabel()
+        header.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        header.setText(self._get_header_text())
+        header_layout = QHBoxLayout()
+        header_layout.addWidget(header)
+        group_box.setLayout(header_layout)
 
-        self.vlayout.addWidget(self.button)
+        #TODO: put in settings
+        compound_htz_tag = "#COMPOUND_HTZ"
+        self.checkbox = QCheckBox(f"Add tag {compound_htz_tag} to all selected variants", self)
+
+        self.button = QPushButton("Select compound htz variants")
+        self.button.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.button.clicked.connect(self._on_click)
+        button_box = QVBoxLayout()
+        button_box.setAlignment(Qt.AlignCenter)
+        button_box.addWidget(self.button)
+
+        self.setWindowTitle("Compound htz filter")
+        self.vlayout = QVBoxLayout()
+        self.vlayout.addWidget(group_box)
+        self.vlayout.addWidget(self.checkbox)
+        self.vlayout.addLayout(button_box)
         self.setLayout(self.vlayout)
 
+    def _get_header_text(self):
+        #TODO: put in settings
+        gene_field = "gnomen"
+        exclusion_criteria = []
 
+        text = f"""<b>Compound heterozygosity</b> is defined in settings as variants with:
+        <ul>
+            <li> sample.gt > 0</li>
+            <li> same content in field: {gene_field}</li>
+        </ul>
+        """
+        if len(exclusion_criteria) > 0:
+            text += "Exclusion criteria:<ul>"
+            for e in exclusion_criteria:
+                text += "<li> {e}</li>"
+            text += "</ul>"
+        return text
 
-    def on_click(self):
-    	print("I'm using this sqlite connection", self.conn)
+    def _get_variant_ids(self):
+        #TODO
+        variants = []
+        return variants
+
+    def _on_click(self):
+        variants = self._get_variant_ids()
+
+        if len(variants) != 0:
+            filters = {"$and": [{"id": {"$in": variants}}]}
+
+            if self.checkbox.checkState():
+                print("checked")
+
+            self.mainwindow.set_state_data("filters", filters)
+            self.mainwindow.refresh_plugins()
+
+        else:
+            QMessageBox.information(
+                self, "No compound htz found", "Variant selection was unchanged"
+            )
+
+        self.close()
 
 if __name__ == "__main__":
     import sys
