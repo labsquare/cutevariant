1c52eeec2f94eb9cc5f9ea97c152d29c18c60c44
Sacha Schutz <sacha@labsquare.org>
2023-04-26 23:15:08 +0200

    test new arch

--- poc/import.py
+++ poc/import.py
@@ -13,20 +13,43 @@ def extract_samples(filename):
             else:
                 return []
 
+    return []
+
 
 def create_tables(conn):
-    conn.sql(
-        "CREATE TABLE IF NOT EXISTS variants(id UINT64 PRIMARY KEY, chrom VARCHAR, pos UINT64, ref VARCHAR, alt VARCHAR)"
-    )
-
-
-def import_variant(conn, filename: str):
-    q = f"""
-    INSERT OR IGNORE INTO variants
-    SELECT hash(CHROM,POS,REF,ALT) as ID, CHROM, POS, REF, ALT FROM 
-    (SELECT "#CHROM" as CHROM, POS,REF, unnest(split(ALT,',')) AS ALT FROM read_csv_auto('{filename}'))
+    q = """
+    CREATE TABLE IF NOT EXISTS variants(
+    id UINT64 PRIMARY KEY,
+    chrom VARCHAR,
+    pos UINT64,
+    ref VARCHAR,
+    alt VARCHAR,
+    count_het UINT32 DEFAULT 0,
+    count_hom UINT32 DEFAULT 0)
     """
+
     conn.sql(q)
+
+
+def vcf_to_parquet(filename: str, output: str):
+    samples = extract_samples(filename)
+
+    gt_field = """CAST(list_aggr(CAST(split(replace(replace(split({0},':')[1],'.','0'),'|','/'),'/') AS UINT8[]),'sum') AS UINT8) AS {0}"""
+    gt_field = ",".join([gt_field.format(sample) for sample in samples])
+
+    print("start conversion")
+    q = f"""
+    COPY (
+    SELECT 
+    "#CHROM" as CHROM,
+    POS,REF,
+    unnest(split(ALT,',')) AS ALT,
+    {gt_field} 
+    FROM read_csv_auto('{filename}',   delim='\t',types={{'#CHROM':'VARCHAR'}})
+    ) TO '{output}'
+    """
+
+    duckdb.sql(q)
 
 
 def create_genotype(name: str, filename: str, output: str):
@@ -44,18 +67,27 @@ if __name__ == "__main__":
 
     conn = duckdb.connect("demo.db")
 
-    duckdb.sql("SET enable_progress_bar=true")
+    duckdb.sql("SET enable_progress_bar=1")
 
-    filename = (
-        "./ALL.chr1.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz"
-    )
+    filename = "./M48.snps.vcf"
 
     print("extract samples ")
     samples = extract_samples(filename)
 
-    print("create tables")
-    create_tables(conn)
-    print("import variants")
-    import_variant(conn, filename)
-    print("create genotypes")
-    create_genotype("NA20127", filename, f"genotypes/NA20127.parquet")
+    # vcf_to_parquet("./M46.snps.vcf", "M46.parquet")
+    vcf_to_parquet("./M46.snps.vcf", "M46.parquet")
+    # vcf_to_parquet("./subset.vcf", "subset.parquet")
+
+    # vcf_to_parquet(
+    #     "./ALL.chr1.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz",
+    #     "huge.parquet",
+    # )
+
+    # print("create tables")
+    # create_tables(conn)
+    # print("import variants")
+    # import_variant(conn, filename)
+    # import_variant(conn, "./M46.snps.vcf")
+
+    # print("create genotypes")
+    # create_genotype("NA20127", filename, f"genotypes/NA20127.parquet")
